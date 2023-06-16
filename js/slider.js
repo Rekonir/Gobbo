52f7fbfc7f17efe814370055f3b99a4890106faf
Rekonir <zhed-nikita@mail.ru>
2023-06-16 17:12:10 +0300

    fix result bugs

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,8 @@
 $(function () {
-    var checkWarcry = $('#WarcryCheck')
-    var result = $('#result')
+    let checkWarcry = $('#WarcryCheck')
+    let result = $('#result')
 
-    var sliderShoot = $('.slider')[0];
+    let sliderShoot = $('.slider')[0];
     noUiSlider.create(sliderShoot, {
         start: [1, 10],
         step: 1,
@@ -13,7 +13,7 @@ $(function () {
             'max': 10
         }
     })
-    var sliderHtH = $('.slider')[1];
+    let sliderHtH = $('.slider')[1];
     noUiSlider.create(sliderHtH, {
         start: [3, 9],
         step: 1,
@@ -24,7 +24,7 @@ $(function () {
             'max': 9
         }
     })
-    var sliderWound = $('.slider')[2];
+    let sliderWound = $('.slider')[2];
     noUiSlider.create(sliderWound, {
         start: [2, 7],
         step: 1,
@@ -35,7 +35,7 @@ $(function () {
             'max': 7
         }
     })
-    var sliderMove = $('.slider')[3];
+    let sliderMove = $('.slider')[3];
     noUiSlider.create(sliderMove, {
         start: [1.5, 10],
         step: 1.5,
@@ -46,7 +46,7 @@ $(function () {
             'max': 10
         }
     })
-    var sliderSize = $('.slider')[4];
+    let sliderSize = $('.slider')[4];
     noUiSlider.create(sliderSize, {
         start: [4.3, 10.8],
         step: 0.5,
@@ -57,8 +57,8 @@ $(function () {
             'max': 10.8
         }
     })
-    var sliderVariety = $('.slider')[5];
-    noUiSlider.create(sliderVariety, {
+    let sliderletiety = $('.slider')[5];
+    noUiSlider.create(sliderletiety, {
         start: [1.2, 10],
         step: 2.2,
         margin: 2.2,
@@ -77,7 +77,7 @@ $(function () {
         Wound: 6,
         Move: 5.1,
         Size: 6.1,
-        Variety: 6.3,
+        letiety: 6.3,
         Warcry: false
 
     }
@@ -88,7 +88,7 @@ $(function () {
         Wound: 6,
         Move: 5.7,
         Size: 5.4,
-        Variety: 2.0,
+        letiety: 2.0,
         Warcry: false
 
     }
@@ -99,7 +99,7 @@ $(function () {
         Wound: 5,
         Move: 5.6,
         Size: 4.8,
-        Variety: 5.1,
+        letiety: 5.1,
         Warcry: false
 
     }
@@ -110,7 +110,7 @@ $(function () {
         Wound: 2,
         Move: 7.6,
         Size: 5.8,
-        Variety: 2.9,
+        letiety: 2.9,
         Warcry: false
 
     }
@@ -121,7 +121,7 @@ $(function () {
         Wound: 4,
         Move: 6.4,
         Size: 6,
-        Variety: 6.1,
+        letiety: 6.1,
         Warcry: false
 
     }
@@ -132,7 +132,7 @@ $(function () {
         Wound: 5,
         Move: 3.2,
         Size: 7.2,
-        Variety: 2.2,
+        letiety: 2.2,
         Warcry: false
 
     }
@@ -143,7 +143,7 @@ $(function () {
         Wound: 5,
         Move: 2.8,
         Size: 6,
-        Variety: 2.7,
+        letiety: 2.7,
         Warcry: false
 
     }
@@ -154,7 +154,7 @@ $(function () {
         Wound: 4,
         Move: 9,
         Size: 6.4,
-        Variety: 2.9,
+        letiety: 2.9,
         Warcry: false
 
     }
@@ -165,7 +165,7 @@ $(function () {
         Wound: 3,
         Move: 8,
         Size: 5.8,
-        Variety: 4.1,
+        letiety: 4.1,
         Warcry: false
 
     }
@@ -176,7 +176,7 @@ $(function () {
         Wound: 2,
         Move: 5.9,
         Size: 6.8,
-        Variety: 8.8,
+        letiety: 8.8,
         Warcry: false
 
     }
@@ -187,7 +187,7 @@ $(function () {
         Wound: 5,
         Move: 7,
         Size: 5.6,
-        Variety: 6.6,
+        letiety: 6.6,
         Warcry: false
 
     }
@@ -198,7 +198,7 @@ $(function () {
         Wound: 5,
         Move: 3,
         Size: 8.2,
-        Variety: 2.2,
+        letiety: 2.2,
         Warcry: true
     }
     const UNTAMED_BEASTS = {
@@ -208,7 +208,7 @@ $(function () {
         Wound: 4,
         Move: 4.6,
         Size: 7.9,
-        Variety: 1.7,
+        letiety: 1.7,
         Warcry: true
     }
     const CORVUS_CABAL = {
@@ -218,7 +218,7 @@ $(function () {
         Wound: 3,
         Move: 5.3,
         Size: 8.3,
-        Variety: 1.5,
+        letiety: 1.5,
         Warcry: true
     }
     const CYPHER_LORDS = {
@@ -228,7 +228,7 @@ $(function () {
         Wound: 3,
         Move: 4.6,
         Size: 7.5,
-        Variety: 1.5,
+        letiety: 1.5,
         Warcry: true
     }
     const THE_UNMADE = {
@@ -238,7 +238,7 @@ $(function () {
         Wound: 3,
         Move: 4.6,
         Size: 7.9,
-        Variety: 1.2,
+        letiety: 1.2,
         Warcry: true
     }
     const SPLINTERED_FANG = {
@@ -248,7 +248,7 @@ $(function () {
         Wound: 4,
         Move: 3.2,
         Size: 9.5,
-        Variety: 2.4,
+        letiety: 2.4,
         Warcry: true
     }
     const SCIONS_OF_THE_FLAME = {
@@ -258,7 +258,7 @@ $(function () {
         Wound: 4,
         Move: 3.2,
         Size: 9.1,
-        Variety: 2.7,
+        letiety: 2.7,
         Warcry: true
     }
     const SPIRE_TYRANTS = {
@@ -268,7 +268,7 @@ $(function () {
         Wound: 3,
         Move: 2.9,
         Size: 9.5,
-        Variety: 2.2,
+        letiety: 2.2,
         Warcry: true
     }
     const DARKOATH_SAVAGERS = {
@@ -278,7 +278,7 @@ $(function () {
         Wound: 3,
         Move: 3,
         Size: 9,
-        Variety: 2.9,
+        letiety: 2.9,
         Warcry: true
     }
     const TARANTULOS_BROOD = {
@@ -288,7 +288,7 @@ $(function () {
         Wound: 2,
         Move: 5,
         Size: 10.8,
-        Variety: 1.7,
+        letiety: 1.7,
         Warcry: true
     }
     const FLESH_EATER_COURTS = {
@@ -298,7 +298,7 @@ $(function () {
         Wound: 6,
         Move: 7.2,
         Size: 4.9,
-        Variety: 2.9,
+        letiety: 2.9,
         Warcry: false
     }
     const NIGHTHAUNT = {
@@ -308,7 +308,7 @@ $(function () {
         Wound: 4,
         Move: 7.1,
         Size: 6.4,
-        Variety: 6.1,
+        letiety: 6.1,
         Warcry: false
     }
     const OSSIARCH_BONEREAPERS = {
@@ -318,7 +318,7 @@ $(function () {
         Wound: 5,
         Move: 5.6,
         Size: 5.7,
-        Variety: 4.4,
+        letiety: 4.4,
         Warcry: false
     }
     const SOULBLIGHT_GRAVELORDS = {
@@ -328,7 +328,7 @@ $(function () {
         Wound: 4,
         Move: 5.1,
         Size: 7,
-        Variety: 5.4,
+        letiety: 5.4,
         Warcry: false
     }
     const GLOOMSPITE_GITZ = {
@@ -338,7 +338,7 @@ $(function () {
         Wound: 3,
         Move: 5.6,
         Size: 7.8,
-        Variety: 6.3,
+        letiety: 6.3,
         Warcry: false
     }
     const BONESPLITTERZ = {
@@ -348,7 +348,7 @@ $(function () {
         Wound: 4,
         Move: 6.4,
         Size: 6.5,
-        Variety: 4.6,
+        letiety: 4.6,
         Warcry: false
     }
     const IRONJAWZ = {
@@ -358,7 +358,7 @@ $(function () {
         Wound: 6,
         Move: 3.4,
         Size: 5.6,
-        Variety: 3.9,
+        letiety: 3.9,
         Warcry: false
     }
     const KRULEBOYZ = {
@@ -368,7 +368,7 @@ $(function () {
         Wound: 4,
         Move: 3.4,
         Size: 7.6,
-        Variety: 3.7,
+        letiety: 3.7,
         Warcry: false
     }
     const OGOR_MAWTRIBES = {
@@ -378,7 +378,7 @@ $(function () {
         Wound: 6,
         Move: 4.7,
         Size: 4.3,
-        Variety: 4.6,
+        letiety: 4.6,
         Warcry: false
     }
     const DAUGHTERS_OF_KHAINE = {
@@ -388,7 +388,7 @@ $(function () {
         Wound: 4,
         Move: 8.2,
         Size: 6.3,
-        Variety: 5.1,
+        letiety: 5.1,
         Warcry: false
     }
     const KHAINITE_SHADOWSTALKERS = {
@@ -398,7 +398,7 @@ $(function () {
         Wound: 2,
         Move: 6.1,
         Size: 8.3,
-        Variety: 1.7,
+        letiety: 1.7,
         Warcry: true
     }
     const FYRESLAYERS = {
@@ -408,7 +408,7 @@ $(function () {
         Wound: 4,
         Move: 1.5,
         Size: 8.8,
-        Variety: 4.6,
+        letiety: 4.6,
         Warcry: false
     }
     const IDONETH_DEEPKIN = {
@@ -418,7 +418,7 @@ $(function () {
         Wound: 4,
         Move: 8.5,
         Size: 5.2,
-        Variety: 2.9,
+        letiety: 2.9,
         Warcry: false
     }
     const KHARADRON_OVERLORDS = {
@@ -428,7 +428,7 @@ $(function () {
         Wound: 5,
         Move: 6.5,
         Size: 6.3,
-        Variety: 6.8,
+        letiety: 6.8,
         Warcry: false
     }
     const LUMINETH_REALM_LORDS = {
@@ -438,7 +438,7 @@ $(function () {
         Wound: 4,
         Move: 10,
         Size: 8.9,
-        Variety: 5.1,
+        letiety: 5.1,
         Warcry: false
     }
     const SERAPHON = {
@@ -448,7 +448,7 @@ $(function () {
         Wound: 4,
         Move: 7,
         Size: 6.4,
-        Variety: 8.3,
+        letiety: 8.3,
         Warcry: false
     }
     const SYLVANETH = {
@@ -458,7 +458,7 @@ $(function () {
         Wound: 4,
         Move: 6.6,
         Size: 6.1,
-        Variety: 5.4,
+        letiety: 5.4,
         Warcry: false
     }
     const CITIES_OF_SIGMAR = {
@@ -468,7 +468,7 @@ $(function () {
         Wound: 4,
         Move: 5.8,
         Size: 7.8,
-        Variety: 10,
+        letiety: 10,
         Warcry: false
     }
     const THUNDERSTRIKE_STORMCASTS = {
@@ -478,7 +478,7 @@ $(function () {
         Wound: 7,
         Move: 3,
         Size: 5.6,
-        Variety: 4.4,
+        letiety: 4.4,
         Warcry: false
     }
     const STORMCAST_ETERNALS_SACROSANCT_CHAMBER = {
@@ -488,7 +488,7 @@ $(function () {
         Wound: 6,
         Move: 5.5,
         Size: 4.6,
-        Variety: 5.9,
+        letiety: 5.9,
         Warcry: false
     }
     const STORMCAST_ETERNALS_VANGUARD_AUXILIARY_CHAMBER = {
@@ -498,7 +498,7 @@ $(function () {
         Wound: 6,
         Move: 8.3,
         Size: 4.5,
-        Variety: 3.9,
+        letiety: 3.9,
         Warcry: false
     }
     const STORMCAST_ETERNALS_WARRIOR_CHAMBER = {
@@ -508,48 +508,50 @@ $(function () {
         Wound: 6,
         Move: 8.4,
         Size: 5.2,
-        Variety: 9,
+        letiety: 9,
         Warcry: false
     }
     const WarcryFraction = [BEASTS_OF_CHAOS, BLADES_OF_KHORNE_DAEMONS, BLADES_OF_KHORNE_BLOODBOUND, DISCIPLES_OF_TZEENTCH_DAEMONS, DISCIPLES_OF_TZEENTCH_ARCANITES, MAGGOTKIN_OF_NURGLE_AEMONS, MAGGOTKIN_OF_NURGLE_ROTBRINGERS, HEDONITES_OF_SLAANESH_DAEMONS, HEDONITES_OF_SLAANESH_SYBARITES, SKAVEN, SLAVES_TO_DARKNESSEN, IRON_GOLEM, UNTAMED_BEASTS, CORVUS_CABAL, CYPHER_LORDS, THE_UNMADE, SPLINTERED_FANG, SCIONS_OF_THE_FLAME, SPIRE_TYRANTS, DARKOATH_SAVAGERS, TARANTULOS_BROOD, IRON_GOLEM, FLESH_EATER_COURTS, NIGHTHAUNT, OSSIARCH_BONEREAPERS, SOULBLIGHT_GRAVELORDS, GLOOMSPITE_GITZ, BONESPLITTERZ, IRONJAWZ, KRULEBOYZ, OGOR_MAWTRIBES, DAUGHTERS_OF_KHAINE, KHAINITE_SHADOWSTALKERS, FYRESLAYERS, IDONETH_DEEPKIN, KHARADRON_OVERLORDS, LUMINETH_REALM_LORDS, SERAPHON, SYLVANETH, CITIES_OF_SIGMAR, THUNDERSTRIKE_STORMCASTS, STORMCAST_ETERNALS_SACROSANCT_CHAMBER, STORMCAST_ETERNALS_VANGUARD_AUXILIARY_CHAMBER, STORMCAST_ETERNALS_WARRIOR_CHAMBER]
-    var ShootResult = []
-    var HtHResult = []
-    var WoundResult = []
-    var MoveResult = []
-    var SizeResult = []
-    var VarietyResult = []
-    var WarcryResult = []
+    let ShootResult = []
+    let HtHResult = []
+    let WoundResult = []
+    let MoveResult = []
+    let SizeResult = []
+    let letietyResult = []
+    let WarcryResult = WarcryFraction.filter(fraction => !fraction.Warcry).map(fraction => fraction.Name)
+    let FullResult = []
 
-    for (var i = 0; i < WarcryFraction.length; i++) {
+    for (let i = 0; i < WarcryFraction.length; i++) {
         ShootResult.push(WarcryFraction[i].Name),
             HtHResult.push(WarcryFraction[i].Name),
             WoundResult.push(WarcryFraction[i].Name),
             MoveResult.push(WarcryFraction[i].Name),
             SizeResult.push(WarcryFraction[i].Name),
-            VarietyResult.push(WarcryFraction[i].Name)
+            letietyResult.push(WarcryFraction[i].Name)
+        FullResult.push(WarcryFraction[i].Name)
     }
 
     // Обработка фильтров//
-    var ShootMin = 2
-    var ShootMax = 10
-    var ShootAll = [ShootMin, ShootMax]
+    let ShootMin = 2
+    let ShootMax = 10
+    let ShootAll = [ShootMin, ShootMax]
     sliderShoot.noUiSlider.on('change', (values, handle) => {
         ShootAll[handle] = Number(values[handle])
         ShootResult = []
-        for (var i = 0; i < WarcryFraction.length; i++) {
+        for (let i = 0; i < WarcryFraction.length; i++) {
             if (WarcryFraction[i].Shoot >= ShootAll[0] && WarcryFraction[i].Shoot <= ShootAll[1]) {
                 ShootResult.push(WarcryFraction[i].Name)
             }
         }
     })
 
-    var HtHMin = 3
-    var HtHMax = 9
-    var HtHAll = [HtHMin, HtHMax]
+    let HtHMin = 3
+    let HtHMax = 9
+    let HtHAll = [HtHMin, HtHMax]
     sliderHtH.noUiSlider.on('change', (values, handle) => {
         HtHAll[handle] = Number(values[handle])
         HtHResult = []
-        for (var i = 0; i < WarcryFraction.length; i++) {
+        for (let i = 0; i < WarcryFraction.length; i++) {
             if (WarcryFraction[i].HtH >= HtHAll[0] && WarcryFraction[i].HtH <= HtHAll[1]) {
                 HtHResult.push(WarcryFraction[i].Name)
             }
@@ -557,84 +559,109 @@ $(function () {
     })
 
 
-    var WoundMin = 2
-    var WoundMax = 7
-    var WoundAll = [WoundMin, WoundMax]
+    let WoundMin = 2
+    let WoundMax = 7
+    let WoundAll = [WoundMin, WoundMax]
     sliderWound.noUiSlider.on('change', (values, handle) => {
         WoundAll[handle] = Number(values[handle])
         WoundResult = []
-        for (var i = 0; i < WarcryFraction.length; i++) {
+        for (let i = 0; i < WarcryFraction.length; i++) {
             if (WarcryFraction[i].Wound >= WoundAll[0] && WarcryFraction[i].Wound <= WoundAll[1]) {
                 WoundResult.push(WarcryFraction[i].Name)
             }
         }
     })
 
-    var MoveMin = 1.5
-    var MoveMax = 10
-    var MoveAll = [MoveMin, MoveMax]
+    let MoveMin = 1.5
+    let MoveMax = 10
+    let MoveAll = [MoveMin, MoveMax]
     sliderMove.noUiSlider.on('change', (values, handle) => {
         MoveAll[handle] = Number(values[handle])
         MoveResult = []
-        for (var i = 0; i < WarcryFraction.length; i++) {
+        for (let i = 0; i < WarcryFraction.length; i++) {
             if (WarcryFraction[i].Move >= MoveAll[0] && WarcryFraction[i].Move <= MoveAll[1]) {
                 MoveResult.push(WarcryFraction[i].Name)
             }
         }
     })
 
-    var SizeMin = 4.3
-    var SizeMax = 10.8
-    var SizeAll = [SizeMin, SizeMax]
+    let SizeMin = 4.3
+    let SizeMax = 10.8
+    let SizeAll = [SizeMin, SizeMax]
     sliderSize.noUiSlider.on('change', (values, handle) => {
         SizeAll[handle] = Number(values[handle])
         SizeResult = []
-        for (var i = 0; i < WarcryFraction.length; i++) {
+        for (let i = 0; i < WarcryFraction.length; i++) {
             if (WarcryFraction[i].Size >= SizeAll[0] && WarcryFraction[i].Size <= SizeAll[1]) {
                 SizeResult.push(WarcryFraction[i].Name)
             }
         }
     })
 
-    var VarietyMin = 1.2
-    var VarietyMax = 10
-    var VarietyAll = [VarietyMin, VarietyMax]
-    sliderVariety.noUiSlider.on('change', (values, handle) => {
-        VarietyAll[handle] = Number(values[handle])
-        VarietyResult = []
-        for (var i = 0; i < WarcryFraction.length; i++) {
-            if (WarcryFraction[i].Variety >= VarietyAll[0] && WarcryFraction[i].Variety <= VarietyAll[1]) {
-                VarietyResult.push(WarcryFraction[i].Name)
+    let letietyMin = 1.2
+    let letietyMax = 10
+    let letietyAll = [letietyMin, letietyMax]
+    sliderletiety.noUiSlider.on('change', (values, handle) => {
+        letietyAll[handle] = Number(values[handle])
+        letietyResult = []
+        for (let i = 0; i < WarcryFraction.length; i++) {
+            if (WarcryFraction[i].letiety >= letietyAll[0] && WarcryFraction[i].letiety <= letietyAll[1]) {
+                letietyResult.push(WarcryFraction[i].Name)
             }
         }
     })
 
     checkWarcry.on('click', () => {
-        WarcryResult = []
-        for (var i = 0; i < WarcryFraction.length; i++) {
-            if (checkWarcry.prop('checked') === false && WarcryFraction[i].Warcry === false) {
-                WarcryResult.push(WarcryFraction[i].Name)
-            }
-            else if (checkWarcry.prop('checked') === true) {
-                WarcryResult.push(WarcryFraction[i].Name)
-            }
-        }
+        WarcryResult = checkWarcry.prop('checked') ?
+            FullResult : WarcryFraction.filter(fraction => !fraction.Warcry).map(fraction => fraction.Name);
+        console.log(WarcryResult)
     })
+    function commonElements(array1, array2, array3, array4, array5, array6, array7) {
+        console.log(FullResult)
 
+        let common = FullResult.filter(x => array1.includes(x))
+        console.log(common)
+
+        common = common.filter(x => array2.includes(x))
+        console.log(common)
+
+        common = common.filter(x => array3.includes(x))
+        console.log(common)
+
+        common = common.filter(x => array4.includes(x))
+        console.log(common)
+
+        common = common.filter(x => array5.includes(x))
+        console.log(common)
+
+        common = common.filter(x => array6.includes(x))
+        console.log(common)
+
+        common = common.filter(x => array7.includes(x))
+        console.log(common)
+
+        return common;
+    }
 
     // Вывод рузельтата
-    var AllResult = []
-    var str = ''
-    $('.content').on('click', () => {
-        AllResult = _.intersection(ShootResult, HtHResult, WoundResult, MoveResult, SizeResult, VarietyResult, WarcryResult)
-        if (AllResult[1] == undefined) {
+    const createResult = () => {
+        let AllResult = []
+        let str = ''
+        let res = [ShootResult, HtHResult, WoundResult, MoveResult, SizeResult, letietyResult, WarcryResult]
+        AllResult = commonElements(ShootResult, HtHResult, WoundResult, MoveResult, SizeResult, letietyResult, WarcryResult)
+        if (AllResult[0] == undefined) {
             AllResult = ['Гоббо ничего не смог найти для тебя.']
         }
-        for (var i = 0; i < AllResult.length; i++) {
+        for (let i = 0; i < AllResult.length; i++) {
             str += `${AllResult[i]} <br> <br>`
         }
         result.html(str)
         str = ''
+        console.log(res, ShootResult.length, HtHResult.length, WoundResult.length, MoveResult.length, SizeResult.length, letietyResult.length, WarcryResult.length)
+    }
+    $('.content').on('click', () => {
+        createResult()
+
     })
 
 
